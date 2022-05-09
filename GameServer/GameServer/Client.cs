e66fdbb8420d08dce53f4234177c8fb9d337ef94
domoster1st8-dn <domoster1st8a@gmail.com>
2022-05-09 10:21:12 +0700

    Initial commit

---
+++ GameServer/GameServer/Client.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Client
+    {
+        public static int dataBufferSize = 4096;
+        public int id;
+        public TCP tcp;
+
+        public Client(int _clientId)
+        {
+            id = _clientId;
+            tcp = new TCP(id);
+        }    
+        
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] recieveBuffer;
+            public TCP(int _id)
+            {
+                id = _id;
+            }
+
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+
+                recieveBuffer = new byte[dataBufferSize];
+                stream.BeginRead(recieveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+                //TODO: send welcome packet
+
+            }
+
+            private void ReceiveCallback(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLenght = stream.EndRead(_result);
+                    if(_byteLenght <= 0)
+                    {
+                        //TODO: Disconnect
+                        return;
+                    }
+
+                    byte[] _data = new byte[_byteLenght];
+                    Array.Copy(recieveBuffer, _data, _byteLenght);
+
+                    //TODO: handle data
+                    stream.BeginRead(recieveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                }
+                catch (Exception _ex)
+                {
+                    Console.WriteLine($"Error receiving TCP data: {_ex}");
+                    //TODO: disconnect
+                }
+            }
+        }
+    }
+}
---
+++ GameServer/GameServer/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int MaxPlayers { get; private set; }
+        public static int Port { get; private set; }
+
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+
+        private static TcpListener tcpListener;
+
+
+        public static void Start(int maxplayers, int port)
+        {
+            MaxPlayers = maxplayers;
+            Port = port;
+            
+            Console.WriteLine("Starting server ...");
+            InitializeServerData();
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Server started on {Port}.");
+        } 
+        private static void TCPConnectCallback(IAsyncResult asyncResult)
+        {
+            TcpClient tcpClient = tcpListener.EndAcceptTcpClient(asyncResult);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Incoming connection from {tcpClient.Client.RemoteEndPoint}...");
+
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                if(clients[i].tcp.socket == null)
+                {
+                    clients[i].tcp.Connect(tcpClient);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{tcpClient.Client.RemoteEndPoint} failed to connect: server full");
+        }
+
+        private static void InitializeServerData()
+        {
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                clients.Add(i, new Client(i));
+            }
+        }
+    }
+}
